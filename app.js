d026ccce6fd0c6016465f471fec79215c87226f7
Adil Naqvi <notadilnaqvi@gmail.com>
2020-08-28 13:02:52 +0500

    Get data from chess.com API

--- app.js
+++ app.js
@@ -1,30 +1,66 @@
-require("dotenv").config();
-const MongoClient = require('mongodb').MongoClient;
+const fetch = require('node-fetch');
 
-URI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster.olekz.gcp.mongodb.net/stats?retryWrites=true&w=majority`;
+// GET DATA FROM CHESS.COM API, PARSE IT, RETURN DICT
+async function getData() {
+    // GET MONTH & YEAR
+    let date = new Date();
+    let month = (_ = (date.getMonth() + 1).toString()).length == 1 ? "0" + _ : _; // MAKE SURE MONTH IS 2 DIGITS (05 NOT 5 FOR MAY)
+    let year = date.getFullYear().toString();
+    const chessUsername = "notadilnaqvi";
 
-const client = new MongoClient(URI, {
-    useUnifiedTopology: true
-});
+    // CALL APIS, GET JSONS
+    let gamesApi = `https://api.chess.com/pub/player/${chessUsername}/games/${year}/${month}`;
+    let statsApi = `https://api.chess.com/pub/player/${chessUsername}/stats`;
+    let gamesResponse = await fetch(gamesApi);
+    let statsResponse = await fetch(statsApi);
+    let gamesJson = await gamesResponse.json();
+    let statsJson = await statsResponse.json();
 
-(async () => {
-    await client.connect();
-    const database = client.db("stats");
-    const collection = database.collection("myChessStats");
-    // let result = await collection.insertOne({
-    //     "month": "August",
-    //     "played": 23,
-    //     "won": 12,
-    //     "rating": 1253
-    // })
-    // console.log(result.insertedId);
-    let cursor = collection.find({});
+    // RETRIEVE RELEVANT DATA FROM JSONS
+    let gamesData = getDataFromGamesJson(gamesJson);
+    let statsData = getDataFromStatsJson(statsJson);
+    
+    // RETURN DICT
+    return({
+        "month": (new Date(Date.now() - 864e5)).toLocaleString('default', {month: 'long'}), // GET PREVIOUS MONTH (NOT CURRENT)
+        "played": gamesData["played"],
+        "won": gamesData["won"],
+        "rating": statsData["rating"]
+    });
+}
 
-    while (await cursor.hasNext()) {
-        let temp = await cursor.next();
-        console.log(temp);
-    }
-    client.close()
-})();
 
-// 
+function getDataFromGamesJson(_json){
+    let gamesPlayed = _json.games.length;
+    let gamesWon = 0;
+    _json.games.forEach(game => {
+        let color = (game.white.username == "notadilnaqvi") ? "white" : "black";
+        if (game[color].result == "win") {gamesWon++};
+    });
+    return {
+        "played": gamesPlayed,
+        "won": gamesWon
+    }  
+}
+
+function getDataFromStatsJson(_json) {
+    return {"rating": _json.chess_blitz.last.rating};
+}
+
+// MAIN
+async function annoyUzair(){
+    data = await getData()
+    console.log(data);
+    // send email
+    // upload data
+    // update website
+}
+
+annoyUzair()
+
+// JOB EVERY MONTH
+// var CronJob = require('cron').CronJob;
+// var job = new CronJob('* * * * * *', function () {
+//     console.log('You will see this message every second');
+// }, null, true, 'Asia/Karachi');
+// job.start();
---
+++ mongo.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const MongoClient = require('mongodb').MongoClient;
+
+URI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster.olekz.gcp.mongodb.net/stats?retryWrites=true&w=majority`;
+
+const client = new MongoClient(URI, {
+    useUnifiedTopology: true
+});
+
+(async () => {
+    await client.connect();
+    const database = client.db("stats");
+    const collection = database.collection("myChessStats");
+    // let result = await collection.insertOne({
+    //     "month": "August",
+    //     "played": 23,
+    //     "won": 12,
+    //     "rating": 1253
+    // })
+    // console.log(result.insertedId);
+    let cursor = collection.find({});
+
+    while (await cursor.hasNext()) {
+        let temp = await cursor.next();
+        console.log(temp);
+    }
+    client.close()
+})();
